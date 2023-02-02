60d854e8d0605037e074df569e3770ead1caab3b
Charlie <charlie.chuangqi.li@gmail.com>
2023-02-02 21:06:24 +1000

    custom hook and hoc c1

--- src/components/RobotDiscount.tsx
+++ src/components/RobotDiscount.tsx
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react'
 import styles from './Robot.module.css';
 import { appContext } from '../AppState';
-import withAddToCart from './AddToCart';
+import { useAddToCart } from './AddToCart';
 interface RobotProps {
     id: number,
     name: string,
     email: string,
     addToCart: (id, name) => void
 }
-const RobotDiscount: React.FC<RobotProps> = ({ id, name, email, addToCart }) => {
+const RobotDiscount: React.FC<RobotProps> = ({ id, name, email }) => {
     const value = useContext(appContext)
+    const addToCart = useAddToCart();
     return (
         <div className={styles.cardContainer}>
             <img alt='robot' src={`https://robohash.org/${id}`} />
@@ -21,4 +22,4 @@ const RobotDiscount: React.FC<RobotProps> = ({ id, name, email, addToCart }) => 
         </div>)
 }
 
-export default withAddToCart(RobotDiscount);
+export default RobotDiscount;
